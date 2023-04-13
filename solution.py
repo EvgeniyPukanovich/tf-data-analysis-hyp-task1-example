04309fd0da4e1b5ef169d783cead7a98f4a22403
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-04-13 12:36:37 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
+chat_id = 970839957
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
+             leads_test: int) -> bool:
+    control_failures = x_cnt - x_success
+    test_failures =  leads_test - y_success
+    table = [[x_success, control_failures], [y_success, test_failures]]
+    row_totals = [sum(row) for row in table]
+    col_totals = [sum(col) for col in zip(*table)]
+    total = sum(table[0]) + sum(table[1])
+    expected = [[row_totals[i] * col_totals[j] / total for j in range(2)] for i in range(2)]
+    chisq, pval = stats.chisquare([table[0][0], table[1][0]], f_exp=[expected[0][0], expected[1][0]])
+    alpha = 0.06
+    return pval < alpha
