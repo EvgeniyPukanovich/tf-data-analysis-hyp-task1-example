5be343220fe7152592fd64313e2e9b73ac007854
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-04-13 12:37:45 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,5 +16,5 @@ def solution(x_success: int,
     total = sum(table[0]) + sum(table[1])
     expected = [[row_totals[i] * col_totals[j] / total for j in range(2)] for i in range(2)]
     chisq, pval = stats.chisquare([table[0][0], table[1][0]], f_exp=[expected[0][0], expected[1][0]])
-    alpha = 0.06
+    alpha = 0.09
     return pval < alpha
